c26d7a2a2516e13631458fa46eea6c7b667e33b2
Scott Mackenzie <scott.mac.web@gmail.com>
2019-09-07 10:30:57 +1200

    jest setup

---
+++ __tests__/fileTest.ts
@@ -0,0 +1,10 @@
+import * as file from '../File';
+import jest from 'jest';
+
+test('fileExists/find file', () => {
+    expect(3).toBe(3);
+});
+
+test('fileExists/find non file', () => {
+    expect(3).toBe(3);
+});
